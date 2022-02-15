014b3926e82cd914c141985cdb4a719119c36649
Georgii Zhulikov <l8doku@gmail.com>
2022-02-15 14:33:30 +0300

    Workshop 11 result 213-2

--- code/src/problem1/main.cpp
+++ code/src/problem1/main.cpp
@@ -12,8 +12,6 @@ using std::endl;
 typedef std::vector<int> VecInt;
 
 // Generating random numbers:
-
-
 VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
 {
     std::random_device rd;
@@ -27,12 +25,42 @@ VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
     // these will be two different random numbers
 }
 
-
-
 // consider using range-based loops for printing
 void printVec(const VecInt& vecInt);
 
+typedef std::map<int, size_t> MapInt;
+
+MapInt fillMap(...)
+{
+    MapInt mapCount;
+    
+    int key = 0;
+    size_t value = 1;
+    mapCount[key] = value;
+    mapCount[key] += 1;
+    
+    mapCount.insert({key, value});
+    
+    std::pair<bool, size_t> outputPair = std::make_pair(1, 2);
+
+    std::map<int, size_t> mapInt;
+    for (const std::pair<const int, size_t>& x : mapInt)
+    {
+        cout << x.first;
+    }
+    // if (mapCount.find(key) == mapCount.end())
+//    {
+//         key NOT in mapCount
+//    }
+}
+
 //void printMap(const MapInt& mapInt)
+
+
+// x.first
+// x.second
+
+// for (int x : vec)
 
 
 
