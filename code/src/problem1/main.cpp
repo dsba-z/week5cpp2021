35ead2f376a3c10eb2ff81093865b0adefb6f7e0
Georgii Zhulikov <l8doku@gmail.com>
2022-02-15 12:39:12 +0300

    Workshop 11 result 212-2

--- code/src/problem1/main.cpp
+++ code/src/problem1/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <random>
-#include <ctime>
 #include <map>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-
 typedef std::vector<int> VecInt;
 
 // Generating random numbers:
-
-
 VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
 {
     std::random_device rd;
@@ -27,14 +23,33 @@ VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
     // these will be two different random numbers
 }
 
-
-
 // consider using range-based loops for printing
 void printVec(const VecInt& vecInt);
 
+// https://en.cppreference.com/w/cpp/container/map/find
+
+// condition is TRUE when element is NOT FOUND
+//if (mapCount.find(key) == mapCount.end())
+
+std::map<int, size_t> fillMap(...)
+{
+    int key = 0;
+    size_t value = 1;
+    std::map<int, size_t> mapCount;
+    // code
+    
+//    mapCount[key] = value;
+    mapCount[key] += 1;
+    
+    mapCount.insert({key, value});
+}
+
 //void printMap(const MapInt& mapInt)
 
-
+// for (int x : vec)
+// for (std::pair<int, size_t> x: mapVariable)
+// x.first - key
+// x.second - value
 
 int main()
 {
