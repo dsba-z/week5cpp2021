f92c1a9465597515a52a28c93194696a0d26b11a
Georgii Zhulikov <l8doku@gmail.com>
2022-02-15 11:02:30 +0300

    Add some initial code

--- code/src/problem1/main.cpp
+++ code/src/problem1/main.cpp
@@ -8,6 +8,34 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+
+typedef std::vector<int> VecInt;
+
+// Generating random numbers:
+
+
+VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distr(rangeStart, rangeEnd - 1);
+    
+    // now use the function call distr(gen) whenever you need a new randon number
+    // for example:
+    int rNum1 = distr(gen);
+    int rNum2 = distr(gen);
+    // these will be two different random numbers
+}
+
+
+
+// consider using range-based loops for printing
+void printVec(const VecInt& vecInt);
+
+//void printMap(const MapInt& mapInt)
+
+
+
 int main()
 {
     cout << "Hello World!" << endl;
